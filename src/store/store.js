cd933d69a2fa2bcddd2716a8ad6cac2414d412d7
sharon-leshan <maleos380@gmail.com>
2022-04-15 19:11:40 +0300

    store.js

---
+++ src/store/store.js
@@ -0,0 +1,12 @@
+import {createStore, combineReducers, applyMiddleware, compose} from 'redux';
+import thunk from 'redux-thunk';
+import authReducer from '../reducers/auth';
+
+const  composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+    combineReducers({
+        auth: authReducer
+    }),
+    composeEnhancers(applyMiddleware(thunk))
+);
+export default store;
